ff232decf5cfbd9b63e586703246aa41f615a68d
Tanmai Kamat <tanmaikamat@gmail.com>
2023-04-06 11:27:26 +0530

    updates

---
+++ Assets/PositonHandler.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.U2D;
+public class PositonHandler : MonoBehaviour
+{
+    
+    // Start is called before the first frame update
+    // SpriteShapeController controller;
+    // Sprite s;
+    void Start()
+    {
+        // s=controller.
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
